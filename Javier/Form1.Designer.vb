b07d391a7eaeae0f6b97f1c7d484c7fc8e88cdb8
Nicolas Alvarez <nicoalvarez9915@gmail.com>
2020-07-17 14:54:50 -0500

    Calculadora

---
+++ Javier/Form1.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TxtTablero = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(93, 253)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "="
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TxtTablero
+        '
+        Me.TxtTablero.Location = New System.Drawing.Point(12, 12)
+        Me.TxtTablero.Name = "TxtTablero"
+        Me.TxtTablero.Size = New System.Drawing.Size(237, 20)
+        Me.TxtTablero.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(174, 152)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "9"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(93, 152)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "8"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 152)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "7"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(174, 123)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "6"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(93, 123)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "5"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 123)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "4"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(174, 94)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "3"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(93, 94)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 23)
+        Me.Button9.TabIndex = 9
+        Me.Button9.Text = "2"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(12, 94)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 10
+        Me.Button10.Text = "1"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(12, 253)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 11
+        Me.Button11.Text = "-"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(12, 224)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(75, 23)
+        Me.Button12.TabIndex = 12
+        Me.Button12.Text = "+"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(174, 253)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(75, 23)
+        Me.Button13.TabIndex = 13
+        Me.Button13.Text = "/"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(174, 224)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(75, 23)
+        Me.Button14.TabIndex = 14
+        Me.Button14.Text = "*"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(93, 181)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(75, 23)
+        Me.Button15.TabIndex = 15
+        Me.Button15.Text = "0"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(93, 65)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(75, 23)
+        Me.Button16.TabIndex = 16
+        Me.Button16.Text = "C"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TxtTablero)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TxtTablero As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+End Class
---
+++ Javier/Form1.vb
@@ -0,0 +1,248 @@
+﻿Imports System.Diagnostics.CodeAnalysis
+
+Public Class Form1
+
+    Dim numero1
+    Dim numero2
+    Dim Reset
+    Dim operacion = "1"
+    Dim aux
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TxtTablero.Text = "Sintax error" Then
+        Else
+            numero2 = Convert.ToInt32(TxtTablero.Text)
+            RealizarOperacion()
+            TxtTablero.Text = numero1
+            operacion = "1"
+        End If
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If (operacion = "1") Then
+            numero1 = Convert.ToInt32(TxtTablero.Text)
+        Else
+            numero2 = Convert.ToInt32(TxtTablero.Text)
+            If numero2 = 0 Then
+                numero1 = "Sintax error"
+            Else
+                numero1 /= numero2
+            End If
+
+        End If
+        TxtTablero.Text = numero1
+        operacion = "Divicion"
+        Reset = "1"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "1"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "1"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "2"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "1"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "3"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "3"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "4"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "4"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "5"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "5"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "6"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "6"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "7"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "7"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "8"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "8"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "9"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "9"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        If (Reset = "1") Then
+            TxtTablero.Text = ""
+            aux = TxtTablero.Text
+            aux += "0"
+            TxtTablero.Text = aux
+            Reset = "2"
+        Else
+            aux = TxtTablero.Text
+            aux += "0"
+            TxtTablero.Text = aux
+        End If
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+
+        If (operacion = "1") Then
+            numero1 = Convert.ToInt32(TxtTablero.Text)
+        Else
+            numero2 = Convert.ToInt32(TxtTablero.Text)
+            numero1 += numero2
+        End If
+        TxtTablero.Text = numero1
+        operacion = "Suma"
+        Reset = "1"
+    End Sub
+
+    Private Sub RealizarOperacion()
+        If operacion = "Suma" Then
+            numero1 += numero2
+        Else
+            If operacion = "Resta" Then
+                numero1 -= numero2
+            Else
+                If operacion = "Multiplicacion" Then
+                    numero1 *= numero2
+                Else
+                    If operacion = "Divicion" Then
+                        If numero2 = 0 Then
+                            numero1 = "Sintax error"
+                        Else
+                            numero1 /= numero2
+                        End If
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If (operacion = "1") Then
+            numero1 = Convert.ToInt32(TxtTablero.Text)
+        Else
+            numero2 = Convert.ToInt32(TxtTablero.Text)
+            numero1 -= numero2
+        End If
+        TxtTablero.Text = numero1
+        operacion = "Resta"
+        Reset = "1"
+    End Sub
+
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If (operacion = "1") Then
+            numero1 = Convert.ToInt32(TxtTablero.Text)
+        Else
+            numero2 = Convert.ToInt32(TxtTablero.Text)
+            numero1 *= numero2
+        End If
+        TxtTablero.Text = numero1
+        operacion = "Multiplicacion"
+        Reset = "1"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        numero1 = 0
+        numero2 = 0
+        operacion = "1"
+        TxtTablero.Text = ""
+    End Sub
+End Class
